d270d10874ddd1d6216f00738770921f5fe954ea
Sujit Roy <sujitroy78148@gmail.com>
2026-02-03 20:13:47 +0530

    update Prisma client import path

--- src/module/organization-profile/organization-profile.controller.js
+++ src/module/organization-profile/organization-profile.controller.js
@@ -1,4 +1,4 @@
-import { Prisma } from "../../generated/prisma/client.js";
+import { Prisma } from "@prisma/client";
 import {
   createOrganizationProfile,
   getOrganizationProfile,
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 import router from "./routes.js";
 
-export const app = express()
+export const app = express();
 
 app.use(express.json());
 app.use(router);
