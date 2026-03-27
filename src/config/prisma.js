73cfcbe5584d6926656495ca9003e1a17c60a03f
Sujit Roy <sujitroy78148@gmail.com>
2026-03-27 17:46:35 +0530

    do error handling of the postgres db connection

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -6,4 +6,13 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
+prisma
+  .$connect()
+  .then(() => {
+    console.log("✅ Database connected successfully");
+  })
+  .catch((err) => {
+    console.error("❌ Database connection failed:", err.message);
+  });
+
 export { prisma };
